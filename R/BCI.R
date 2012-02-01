7cda512df822da572e3c331ac1b32c9320c6a1da
Corey Chivers <corey.chivers@mail.mcgill.ca>
2012-02-01 17:40:56 -0500

    Initial commit to set up repo

---
+++ R/BCI.R
@@ -0,0 +1,21 @@
+BCI <-
+function(mcmc_object,interval=c(0.025,0.975))
+{
+    num_params<-length(mcmc_object$par_names)
+    if(num_params > 1)
+    {
+        bci<-array(dim=c(num_params,length(interval)))
+        for(i in 1:num_params)
+        {
+            bci[i,]<-quantile(mcmc_object$trace[,i],probs=interval)
+        }
+        rownames(bci)<-mcmc_object$par_names
+        colnames(bci)<-as.character(interval)
+    }
+    if(num_params == 1)
+    {  
+            bci<-quantile(mcmc_object$trace,probs=interval)
+    }
+    return(bci)
+}
+
