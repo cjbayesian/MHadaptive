281560e7f3cbef19eddcf1642879042af6183b63
Corey Chivers <corey.chivers@mail.mcgill.ca>
2012-03-05 19:16:52 -0500

    fixed post_mean  (atomic vector) -> vector length(pars)

--- R/BCI.R
+++ R/BCI.R
@@ -5,10 +5,11 @@ function(mcmc_object,interval=c(0.025,0.975))
     if(num_params > 1)
     {
         bci<-array(dim=c(num_params,length(interval)))
+        post_mean<-numeric(num_params)
         for(i in 1:num_params)
         {
             bci[i,]<-quantile(mcmc_object$trace[,i],probs=interval)
-            post_mean<-mean(mcmc_object$trace[,i])
+            post_mean[i]<-mean(mcmc_object$trace[,i])
         }
         rownames(bci)<-mcmc_object$par_names
         colnames(bci)<-as.character(interval)
